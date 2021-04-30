e65d03a7fafdc1e72578ad4a0b7d34b9ab8353da
Imperiali <igor.imperiali@gmail.com>
2021-04-30 08:13:44 -0300

    fix game

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
-theBoard = dict([(idx, ' ') for idx in range(1, 10)])
+THE_BOARD = dict([(idx, ' ') for idx in range(1, 10)])
 
 victories = [
     (1, 2, 3),
     (4, 5, 6),
     (7, 8, 9),
-    (1, 2, 3),
-    (4, 5, 6),
-    (7, 8, 9),
     (1, 5, 9),
     (7, 5, 3),
+    (1, 4, 7),
+    (2, 5, 8),
+    (3, 6, 9),
 ]
 
 
-def print_board(board):
-    print(f"{board[7]} | {board[8]} | {board[9]}")
+def print_board():
+    print(f"{THE_BOARD[7]} | {THE_BOARD[8]} | {THE_BOARD[9]}")
     print("-"*9)
-    print(f"{board[4]} | {board[5]} | {board[6]}")
+    print(f"{THE_BOARD[4]} | {THE_BOARD[5]} | {THE_BOARD[6]}")
     print("-"*9)
-    print(f"{board[3]} | {board[2]} | {board[1]}")
+    print(f"{THE_BOARD[3]} | {THE_BOARD[2]} | {THE_BOARD[1]}")
 
 
-def has_finished(board):
-    return all([value != " " for value in board.values()])
+def has_finished():
+    return all([value != " " for value in THE_BOARD.values()])
 
 
 def check_sequency(positions):
     i, j, h = positions
-    if theBoard[i] == theBoard[j] == theBoard[h] != " ":
+    if THE_BOARD[i] == THE_BOARD[j] == THE_BOARD[h] != " ":
         return True
     return False
 
@@ -44,7 +44,7 @@ def has_winner():
 
 def game():
     turn = "X"
-    while not has_finished(theBoard):
+    while not has_finished():
         # Inicia a vez
         print()
         print(f"Jogador {turn} é a sua vez")
@@ -54,13 +54,13 @@ def game():
             print("Posição invalida")
             continue
 
-        if theBoard[move] == " ":
-            theBoard[move] = turn
+        if THE_BOARD[move] == " ":
+            THE_BOARD[move] = turn
         else:
             print("Posição já jogada")
             continue
 
-        print(print_board(theBoard))
+        print(print_board())
 
         if has_winner():
             print_end_game(turn)
